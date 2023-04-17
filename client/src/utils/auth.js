490f679cbeae1d5910f0961a89490af8f5102c5d
Av720 <120581801+Av720@users.noreply.github.com>
2023-04-17 00:24:47 -0600

    added the Auth vairable and called it to Authservice class

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -2,13 +2,13 @@
 import decode from 'jwt-decode';
 
 // create a new class to instantiate for a user
-class AuthService {
+class AuthService{
   // get user data
   getProfile() {
     return decode(this.getToken());
   }
 
-  // check if user's logged in
+  // check if user is logged in
   loggedIn() {
     // Checks if there is a saved token and it's still valid
     const token = this.getToken();
@@ -46,4 +46,6 @@ class AuthService {
   }
 }
 
-export default new AuthService();
+const Auth = new AuthService();
+
+export default Auth;
