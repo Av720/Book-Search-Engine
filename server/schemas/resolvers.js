80946d8f7938d4c902a20c4db3a6afe9c72b2781
Av720 <120581801+Av720@users.noreply.github.com>
2023-04-17 22:37:54 -0600

    refactored the entire folder to mirror mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,69 +1,74 @@
-const { User } = require('../models');
-const { AuthenticationError } = require('apollo-server-express');
-const { signToken } = require('../utils/auth');
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
     Query: {
-        me: async (_parent, _args, context) => {
+        me: async (parent, args, context) => {
             if (context.user) {
                 const userData = await User.findOne({ _id: context.user._id })
-                    .select('-__v -password');
+                    .select("-__v -password")
+                    .populate("savedBooks");
 
                 return userData;
             }
-            throw new AuthenticationError('You need to be logged in!');
-        }
+            throw new AuthenticationError("You are not logged in");
+        },
     },
     Mutation: {
-        addUser: async (_parent, args) => {
-            const user = await User.create(args);
-            const token = signToken(user);
-
-            return { token, user };
-
-        },
-        login: async (_parent, { email, password }) => {
+        // input the args email and password to the user log in
+        login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
 
             if (!user) {
-                throw new AuthenticationError('User not Found!');
+                throw new AuthenticationError("username/password is incorrect");
             }
-
+            // retrieve and check user password behind the curtain
             const correctPw = await user.isCorrectPassword(password);
 
             if (!correctPw) {
-                throw new AuthenticationError('Incorrect Password!');
+                throw new AuthenticationError("username/password is incorrect");
             }
-
+            // all pass assign token to user.
+            const token = signToken(user);
+            return { token, user };
+        },
+        // input the args username, email and password to add a new user 
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
             const token = signToken(user);
             return { token, user };
         },
 
-        saveBook: async (_parent, { bookData }, context) => {
-            // if (context.user) {
-            const updatedUser = await User.findByIdAndUpdate(
-                { _id: "643cabe8fed56765f185f8ff "},
-                    // { _id: context.user._id }, 
+        // input arg, newbook to pull from parameter 
+        saveBook: async (parent, { newBook }, context) => {
 
-                    { $push: { savedBooks: bookData } },
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: newBook } },
                     { new: true }
                 );
+
                 return updatedUser;
-            // }
-            // throw new AuthenticationError('You need to be logged in!');
+            }
+            throw new AuthenticationError("You must be logged in!");
         },
-        removeBook: async (_parent, { bookId }, context) => {
+        // bookid arg will delete the bookid and all context 
+        removeBook: async (parent, { bookId }, context) => {
+
             if (context.user) {
                 const updatedUser = await User.findOneAndUpdate(
                     { _id: context.user._id },
                     { $pull: { savedBooks: { bookId } } },
                     { new: true }
                 );
+
                 return updatedUser;
             }
-            throw new AuthenticationError('You need to be logged in!');
-        }
-    }
+            throw new AuthenticationError("You must be logged in!");
+        },
+    },
 };
 
 module.exports = resolvers;
