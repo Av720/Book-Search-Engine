d34d6f62690c706aeb5e0cdf694e254d6e55e0d9
Av720 <120581801+Av720@users.noreply.github.com>
2023-04-18 09:42:36 -0600

    deleted .env

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-require ('dotenv').config({path: '../env'})
 
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
